cb5adaf3d67148a1b3a81a45b19e560fe7fcb18d
tilen <tilen.hostnik@gmail.com>
2024-10-18 13:14:33 +0200

    adding user authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,13 +9,13 @@ export class AuthController{
 
     @Post("signup")
     signup(@Body() dto: AuthDto){
-        console.log({
-            dto
-        });
-        return this.authService.signup;
+       console.log({
+        dto
+       });
+        return this.authService.signup(dto);
     }
     @Post("signin")
-    signin(){
-        return this.authService.signin;
+    signin(@Body() dto: AuthDto){
+        return this.authService.signin(dto);
     }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,12 @@
-export interface AuthDto{
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+
+export class AuthDto{
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    //cc
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
